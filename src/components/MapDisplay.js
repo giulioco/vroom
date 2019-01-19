20f56be136537c72db1afcb382cd055d80fa14f5
Pranav Salunke <pranav.salunke@gmail.com>
2019-01-19 13:56:12 -0800

    map page (map does not show; packages are there

---
+++ src/components/MapDisplay.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Route, Link } from 'react-router-dom';
+
+export default class MapDisplay extends React.Component {
+
+  componentDidMount() {
+  }
+
+  render() {
+
+    return (
+      <div>
+        <h1>PUT MAP HERE</h1>
+      </div>
+    );
+  }
+}
