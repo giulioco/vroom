fb5d2383ee7d03732ed2bbd9ffb9e487df4488e4
Pranav Salunke <pranav.salunke@gmail.com>
2019-01-20 00:39:29 -0800

    ViewListing queries  hardcoded id, and it works (does not show anything yet)

--- src/components/ViewListing.js
+++ src/components/ViewListing.js
@@ -2,18 +2,56 @@ import React from 'react';
 import { Route, Link } from 'react-router-dom';
 
 import * as db from '../db';
+import { thisExpression } from 'babel-types';
 
 
-export default class Listing extends React.Component {
+export default class ViewListing extends React.Component {
+
+  state = {
+    data: null
+  }
+
+  componentDidMount() {
+    // this.props.match.params.id
+    db.listings.doc('Bld0KE7qwR6a4Eq1DhAC').get().then((doc) => {
+      if (doc.exists) {
+        var docData = doc.data()
+        console.log(docData)
+        this.setState({ data: docData })
+        // this.state = {
+        //   docExist: true,
+        //   docData
+        // }
+
+      } else {
+        console.log("The Listing you are looking for does not exist")
+        this.setState({ data: null })
+        // this.state = {
+        //   docExist: false
+        // }
+      }
+
+    })
+  }
 
   render() {
-    const { name } = this.props.data;
+    // const { name } = this.props.data;
 
-    return (
-      <div>
-        <h1>View listing</h1>
-        
-      </div>
-    );
+
+    if (this.state.data != null) {
+      return (
+        <div>
+          <h1>View listing {this.props.match.params.id}</h1>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <h1>The listing you are looking for does not exist</h1>
+
+        </div>
+      );
+    }
+
   }
 }
--- src/db.js
+++ src/db.js
@@ -48,72 +48,51 @@ const fetchInfo = () => new Promise((resolve) => {
 });
 
 export const init = () => firestore.enablePersistence()
-.catch((err) => {
-  if (err.code === 'failed-precondition')
-    console.warn('Failed to initialize caching because multiple sessions are open');
-  else
-    console.error(err);
-})
-.then(new Promise((resolve) => {
-  const unsubscribe = auth.onAuthStateChanged(() => {
-    console.log('Signed in:', !!auth.currentUser);
-    unsubscribe();
-    resolve();
-  }, (err) => {
-    console.error('Sign in error:', err);
-    unsubscribe();
-    resolve();
-  });
-}))
-.then(() => {
-  users = firestore.collection('users');
-  listings = firestore.collection('listings');
+  .catch((err) => {
+    if (err.code === 'failed-precondition')
+      console.warn('Failed to initialize caching because multiple sessions are open');
+    else
+      console.error(err);
+  })
+  .then(new Promise((resolve) => {
+    const unsubscribe = auth.onAuthStateChanged(() => {
+      console.log('Signed in:', !!auth.currentUser);
+      unsubscribe();
+      resolve();
+    }, (err) => {
+      console.error('Sign in error:', err);
+      unsubscribe();
+      resolve();
+    });
+  }))
+  .then(() => {
+    users = firestore.collection('users');
+    listings = firestore.collection('listings');
 
-  return fetchInfo();
-});
+    return fetchInfo();
+  });
 
 const googleProvider = new firebase.auth.GoogleAuthProvider();
 
 export const signIn = () => auth.signInWithPopup(googleProvider)
-.then(() => new Promise(resolve => setTimeout(() => resolve(), 1500)))
-.then(() => fetchInfo())
-.then((path) => {
-  if (path) return path;
+  .then(() => new Promise(resolve => setTimeout(() => resolve(), 1500)))
+  .then(() => fetchInfo())
+  .then((path) => {
+    if (path) return path;
 
-  const { from } = decodeQuery(window.location.search);
-  if (from && from.startsWith('/')) return from;
-  else if (window.location.pathname === '/') return '/dashboard';
-  else return window.location.pathname;
-});
+    const { from } = decodeQuery(window.location.search);
+    if (from && from.startsWith('/')) return from;
+    else if (window.location.pathname === '/') return '/dashboard';
+    else return window.location.pathname;
+  });
 
 export const authChange = (fn) => auth.onAuthStateChanged(fn);
 
 export const signOut = () => auth.signOut();
 
 export const deleteProfile = () => auth.signInWithPopup(googleProvider)
-.then(() => auth.currentUser.delete())
-.then(() => auth.signOut())
-.then(() => alert('Account successfully deleted'));
+  .then(() => auth.currentUser.delete())
+  .then(() => auth.signOut())
+  .then(() => alert('Account successfully deleted'));
 
 export const setupAccount = (data) => users.doc(getUser().uid).set(data);
-
-// sortBy can be 'distance' or 'rate'
-// export const getListings = (radius, sortBy = 'rate', fn) => {
-//   let query = listings.orderBy(sortBy);
-//   // listings.where()
-//   return query.onSnapshot((snap) => {
-//     fn(snap.docs.map((doc) => {
-//       const data = doc.data();
-//       data.id = doc.id;
-//       return data;
-//     }));
-//   }, () => fn(null));
-// };
-
-// export const getListing = (id, fn) => {
-//   return listings.doc(id).onSnapshot((doc) => {
-//     const data = doc.data();
-//     data.id = doc.id;
-//     fn(data);
-//   }, () => fn(null));
-// };
