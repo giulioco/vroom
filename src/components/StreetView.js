5e67ccc0595228891d96cb04b07b1e85f3becf31
Wyatt Ades <wyattades@gmail.com>
2019-02-04 23:18:04 -0800

    Using static streetview for now

--- src/components/StreetView.js
+++ src/components/StreetView.js
@@ -1,4 +1,5 @@
 import React from 'react';
+// import ReactStreetview from 'react-streetview';
 
 import { encodeQuery } from '../utils';
 import { LazyImg } from './misc';
@@ -13,12 +14,19 @@ export default class StreetView extends React.PureComponent {
 
     const query = {
       key: GMAPS_TOKEN,
-      location: `${pos.latitude},123${pos.longitude}`,
+      location: `${pos.latitude},${pos.longitude}`,
       size: '400x300',
     };
     const src = `https://maps.googleapis.com/maps/api/streetview?${encodeQuery(query)}`;
 
     return (
+      // <div style={{ width: 'auto', height: 180, background: '#eee' }}>
+      //   <ReactStreetview apiKey={GMAPS_TOKEN} streetViewPanoramaOptions={{
+      //     position: { lat: pos.latitude, lng: pos.longitude },
+      //     pov: { heading: 100, pitch: 0 },
+      //     zoom: 1,
+      //   }}/>
+      // </div>
       <LazyImg src={src} style={{ height: 180, background: '#eee' }}/>
     );
   }
